75c6fdd5c9d0158e1a935be4e55e7055c3cc0b76
Dan Gowans <dangowans@users.noreply.github.com>
2025-09-06 10:24:23 -0400

    fix swing location

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for offline functionality
-const CACHE_NAME = 'scavenger-hunt-v20250906.1';
+const CACHE_NAME = 'scavenger-hunt-v20250906.2';
 
 const rootUrl = 'https://dangowans.github.io/scavenger-hunt';
 
